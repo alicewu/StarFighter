5f576fe3057fb600cfef304c4c7fea383fcdd95a
Alice <alice_wu@mit.edu>
2013-12-09 21:54:16 -0500

    Initial Commit

---
+++ src/StarFighter/Locatable.java
@@ -0,0 +1,47 @@
+package StarFighter;
+
+
+
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+/**
+ * Interface for Locatable objects containing methods to set and get the position of an object
+ * @author Alice
+ */
+public interface Locatable
+{
+    /**
+     * Abstract method to set an object's position to (x,y)
+     * @param x Inputted x coordinate
+     * @param y Inputted y coordinate
+     */
+    public void setPos( int x, int y);
+    
+    /**
+     * Abstract method to set an object's x position
+     * @param x Inputted x coordinate
+     */
+    public void setX( int x );
+    
+    /**
+     * Abstract method to set an object's y position
+     * @param y Inputted y coordinate
+     */
+    public void setY( int y );
+    
+    /**
+     * Abstract method to get an object's x position
+     * @return X coordinate
+     */
+    public int getX();
+    
+    /**
+     * Abstract method to get an object's y position
+     * @return Y coordinate
+     */
+    public int getY();
+}
---
+++ src/StarFighter/Shiptest.java
@@ -0,0 +1,38 @@
+package StarFighter;
+
+
+
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.io.*;
+import java.util.*;
+import static java.lang.System.*;
+
+/**
+ * Runs Ship to test accuracy.  Contains main method.
+ * @author Alice
+ */
+public class Shiptest
+{
+    /**
+     * Main method to run Ship to test accuracy
+     * @param args The standard default array for main methods
+     */
+    public static void main( String args[] )
+    {
+        MovingThing test = new Ship();
+        System.out.println("Ship 1 " + test.toString());
+
+        Ship test2 = new Ship(50,75);
+        System.out.println("Ship 2 " + test2.toString());
+
+        Ship test3 = new Ship();
+        test3.setX(3);
+        test3.setY(5);
+        System.out.println("Ship 3 " + test3.toString());
+    }
+}
